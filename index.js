620a554c0c0d16369b3b7ee564b25878f0bde155
Harsh Patel <thepatel5324@gmail.com>
2023-07-13 12:17:24 +0530

    cklesnmdenmfokew

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.get("*", (req, res) => {
 });
 
 // if not in production use the port 5000
-const PORT = process.env.PORT || 3000;
+const PORT = 80;
 
 console.log("server started on port:", PORT);
 app.listen(PORT);
